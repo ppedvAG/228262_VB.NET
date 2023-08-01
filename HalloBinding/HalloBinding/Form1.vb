8f3bc0ef42a85c7ef967bb5d8a2d1b8fbdf0f743
André <AndreR@ppedv.de>
2023-08-01 16:19:03 +0200

    Hallo Binding

---
+++ HalloBinding/HalloBinding/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+
+
+    Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+        TextBox2.DataBindings.Add("Text", TextBox1, "Text", True, DataSourceUpdateMode.OnPropertyChanged)
+        TextBox2.DataBindings.Add("BackColor", TextBox1, "Text", True, DataSourceUpdateMode.OnPropertyChanged)
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox2.BackColor = Color.Coral
+    End Sub
+End Class
---
+++ HalloBinding/HalloBinding/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("HalloBinding")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("HalloBinding")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("db544b9f-4212-47c6-a833-794b71c5eea3")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
